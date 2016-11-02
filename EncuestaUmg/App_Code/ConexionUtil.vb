7d13a0a31df7a5a0d425af6a3a176ef2f6d0040c
Oscar Enríquez <oscar.enriquez.torre@gmail.com>
2016-11-02 06:41:31 -0600

    First Commit;

---
+++ EncuestaUmg/App_Code/ConexionUtil.vb
@@ -0,0 +1,45 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Data.OleDb
+
+Public Module ConexionUtil
+    Private connection As OleDbConnection
+    Private connectionString As String = "Provider=sqloledb;Data Source=localhost\SQLEXPRESS;Initial Catalog=encuesta;Trusted_Connection=yes"
+
+
+    Public Function GetConnection() As OleDbConnection
+        If connection Is Nothing Then
+            connection = New OleDbConnection(connectionString)
+        End If
+        Return connection
+    End Function
+
+    Public Function CreateCommand(queryString As String, ByRef connection As OleDbConnection, parameters As List(Of OleDbParameter)) As Integer
+        Try
+            Dim command As New OleDbCommand(queryString, connection)
+
+            For Each parameter In parameters
+                command.Parameters.Add(parameter)
+            Next
+            Return command.ExecuteNonQuery
+        Catch ex As Exception
+            Console.Write(ex.StackTrace)
+            Throw New Exception("No fue posible realizar la transaccion, Msg: " & ex.Message)
+        End Try
+    End Function
+
+    Public Function CreateCommandRead(queryString As String, ByRef connection As OleDbConnection, parameters As List(Of OleDbParameter)) As OleDbDataReader
+        Try
+            Dim command As New OleDbCommand(queryString, connection)
+
+            For Each parameter In parameters
+                command.Parameters.Add(parameter)
+            Next
+            Return command.ExecuteReader
+        Catch ex As Exception
+            Console.Write(ex.StackTrace)
+            Throw New Exception("No fue posible realizar la transaccion, Msg: " & ex.Message)
+        End Try
+    End Function
+
+End Module
+
---
+++ EncuestaUmg/Default.aspx.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Data.OleDb
+Imports ConexionUtil
+
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Label1_Load(sender As Object, e As EventArgs) Handles Label1.Load
+        Dim connection As OleDbConnection = ConexionUtil.GetConnection
+        Dim sbSql As String = "SELECT id, login, password FROM usuarios;"
+        Dim parameters As List(Of OleDbParameter) = New List(Of OleDbParameter)
+
+        Try
+            connection.Open()
+            Dim reader = ConexionUtil.CreateCommandRead(sbSql, connection, parameters)
+            If reader.HasRows Then
+                Do While reader.Read
+                    Console.WriteLine("id: " & reader.GetInt64(0) & ", login " & reader.GetString(1))
+                Loop
+            End If
+
+            reader.Close()
+        Catch ex As Exception
+            Console.WriteLine(ex.StackTrace)
+            Throw New Exception("CarreraADO, " & ex.Message)
+        Finally
+            If (connection IsNot Nothing) Then
+                connection.Close()
+            End If
+        End Try
+
+    End Sub
+End Class
